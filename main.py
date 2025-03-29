c3e4395803cb6e843266687733dda000fcefbb43
Ádson Viana <adsonaviana@gmail.com>
2025-03-29 12:37:53 -0300

    Commit de main.py com todas as classes separadas

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #Looping separado
 import pygame
 from pygame.locals import *
-from class_Player import Player
-from class_Obstaculos import Obstaculos
-from class_Looping import Looping
+from Player_class import Player
+from Obstaculos_class import Obstaculos
+from Looping_class import Looping
 
 pygame.init()
 icone_path = 'Sources/player.png'
 icone = pygame.image.load(icone_path)
 pygame.display.set_icon(icone)
-pygame.display.set_caption('Água e fogo no cin, sem água e fogo')
+pygame.display.set_caption('Água e fogo no CIn, sem água e sem fogo')
 
 iniciar = Looping()
 iniciar.f_looping()
