a580bab6f03275d47bb8e5067a0556c0dcd1886e
Giovanna <giovannabardi12@gmail.com>
2025-03-31 12:37:42 -0300

    Update botão.py

--- botão.py
+++ botão.py
@@ -2,20 +2,21 @@ import pygame
 import sys
 
 pygame.init()
-tela = pygame.display.set_mode((800,800))  #PRECISA ALTERAR ISSO DAQUI É A TELA
-pygame.display.set_caption("botao!")
-fonte_principal= pygame.font.SysFont("Cambria", 50)
 
-class botao():
-    #PQ GET RECT NÃO É RECONHECIDO
+tela = pygame.display.set_mode((700,450))
+pygame.display.set_caption("botao!")
+fonte_principal= pygame.font.SysFont("cambria", 50)
+
+class Botao():
     def __init__(self, imagem, x_pos, y_pos, texto_input):
         self.imagem = imagem
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.rect = self.imagem.get_rect(centro=(self.x_pos, self.y_pos)) #centro do botão na pos x e y
+        self.rect = self.imagem.get_rect(center=(self.x_pos, self.y_pos)) #centro do botão na pos x e y
         self.texto_input = texto_input
-        self.texto = fonte_principal.render(self.texto_input, True, "white") #texto que vê na tela
-        self.texto_rect = self.texto_input.get_rec(centro=(self.x_pos, self.y_pos))
+        self.texto = fonte_principal.render(self.texto_input, True, "white")
+        self.texto_rect = self.texto.get_rect(center = (self.x_pos, self.y_pos))#texto que vê na tela
+
 
     def uptade(self): #uptade method - colocar botões na tela
         tela.blit(self.imagem, self.rect) 
@@ -27,13 +28,6 @@ class botao():
 
     def changecolor(self, posicao):
         if posicao[0] in range(self.rect.left, self.rect.right) and posicao[1] in range(self.rect.top, self.rect.bottom): #0=x e 1=y
-            self.texto = fonte_principal.render(self.texto_input, True, "green")
+            self.texto = fonte_principal.render(self.texto_input, True, "gray")
         else:
             self.texto = fonte_principal.render(self.texto_input, True, "white")
-
-
-            
-        
-        
-    
-        
