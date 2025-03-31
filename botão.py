4692e468ae687ae75e9a311e66cef68f9e80c1d3
Giovanna <giovannabardi12@gmail.com>
2025-03-31 08:20:33 -0300

    Add files via upload

---
+++ botão.py
@@ -0,0 +1,39 @@
+import pygame
+import sys
+
+pygame.init()
+tela = pygame.display.set_mode((800,800))  #PRECISA ALTERAR ISSO DAQUI É A TELA
+pygame.display.set_caption("botao!")
+fonte_principal= pygame.font.SysFont("Cambria", 50)
+
+class botao():
+    #PQ GET RECT NÃO É RECONHECIDO
+    def __init__(self, imagem, x_pos, y_pos, texto_input):
+        self.imagem = imagem
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.imagem.get_rect(centro=(self.x_pos, self.y_pos)) #centro do botão na pos x e y
+        self.texto_input = texto_input
+        self.texto = fonte_principal.render(self.texto_input, True, "white") #texto que vê na tela
+        self.texto_rect = self.texto_input.get_rec(centro=(self.x_pos, self.y_pos))
+
+    def uptade(self): #uptade method - colocar botões na tela
+        tela.blit(self.imagem, self.rect) 
+        tela.blit(self.texto, self.texto_rect)
+
+    def check_for_input(self, posicao): #onde o mouse clica
+        if posicao[0] in range(self.rect.left, self.rect.right) and posicao[1] in range(self.rect.top, self.rect.bottom): #0=x e 1=y
+            print("botao pressionado")
+
+    def changecolor(self, posicao):
+        if posicao[0] in range(self.rect.left, self.rect.right) and posicao[1] in range(self.rect.top, self.rect.bottom): #0=x e 1=y
+            self.texto = fonte_principal.render(self.texto_input, True, "green")
+        else:
+            self.texto = fonte_principal.render(self.texto_input, True, "white")
+
+
+            
+        
+        
+    
+        
