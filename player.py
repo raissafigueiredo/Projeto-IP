021ad5211245b9a3cd508990dccc7252d33aa15b
romero003 <153675281+romero003@users.noreply.github.com>
2025-04-02 09:39:56 -0300

    Sprite e pulo atualizados

--- player.py
+++ player.py
@@ -1,95 +1,105 @@
-import pygame
 from mundo import *
+import pygame
+tela = pygame.display.set_mode((700, 450))
 
+# Classe player
 class Player():
     def __init__(self, x, y):
-                img = player_img
-                self.image = pygame.transform.scale(img, (36 ,60))
-                self.rect = self.image.get_rect()
-                self.rect.x = x
-                self.rect.y = y
-                self.vel_y = 0
-                self.jumped = False
-                self.index = 0
-                self.width = self.image.get_width()
-                self.height = self.image.get_height()
-                self.direction = 0
+        # Imagem do player
+        self.imagem_player = pygame.image.load('Sources/personagem_andar_0.png')
+        self.rect = self.imagem_player.get_rect()
 
-    def update(self, dx, dy, colisao_livro_um, colisao_placa_um, colisao_livro_dois, colisao_placa_dois, mundo, tela):
-                plataformas = mundo.plataformas
-                self.dx = dx
-                self.dy = dy
-                self.colisao_livro_um = colisao_livro_um
-                self.colisao_placa_um = colisao_placa_um
-                self.colisao_livro_dois = colisao_livro_dois
-                self.colisao_placa_dois = colisao_placa_dois
-                key = pygame.key.get_pressed()
+        # Criação da lista com sprites
+        self.direcao = 1
+        self.sprites_direita = []
+        self.sprites_esquerda = []
+        
+        for num in range(4):
+            img_right = pygame.image.load(f'Sources/personagem_andar_{num}.png')
+            self.sprites_direita.append(pygame.transform.scale(img_right, (30, 60)))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.sprites_esquerda.append(pygame.transform.scale(img_left, (30, 60)))
 
-                walk_cooldown = 5
+        self.index = 0
 
-                if key[pygame.K_SPACE] and self.jumped == False:
-                    self.vel_y = -10
-                    self.jumped = True
+        # Coordenadas iniciais do player
+        self.rect.x = x
+        self.rect.y = y
+        
+        # Propriedades de movimento
+        self.gravity = 0
+        self.speed_x = 5
+        self.speed_y = 0
+        self.no_chao = False  
 
-                if key[pygame.K_SPACE] == False:
-                    self.jumped = False
+        # Medidas da imagem
+        self.width = self.imagem_player.get_width()
+        self.height = self.imagem_player.get_height()
+
+    def update(self, mundo):
+        # Importa lista de plataformas
+        plataformas = mundo.plataformas
+
+        # Checa se as teclas estão pressionadas
+        key = pygame.key.get_pressed()
                 
+        if key[pygame.K_a]:
+            self.direcao = 0
+            self.rect.x -= self.speed_x
+            self.index += 0.2
+            if self.index >= len(self.sprites_esquerda):
+                self.index = 1
+            self.imagem_player = self.sprites_esquerda[int(self.index)]
 
-                
-                if key[pygame.K_LEFT]:
+        if key[pygame.K_d]:
+            self.direcao = 1
+            self.rect.x += self.speed_x
+            self.index += 0.2
+            if self.index >= len(self.sprites_direita):
+                self.index = 1
+            self.imagem_player = self.sprites_direita[int(self.index)]
 
-                    if self.colisao_livro_um == "à direita do obstaculo" or self.colisao_placa_um == "à direita do obstaculo" or self.colisao_livro_dois == "à direita do obstaculo" or self.colisao_placa_dois == "à direita do obstaculo":
-                     pass
-                    else:
-                     self.dx -= 5
+        if not key[pygame.K_a] and not key[pygame.K_d]:
+            self.index = 0
+            self.imagem_player = self.sprites_direita[self.index] if self.direcao == 1 else self.sprites_esquerda[self.index]
 
+        # Aplica gravidade
+        self.gravity += 1
+        if self.gravity > 15:
+            self.gravity = 15
 
+        # Configura o botao do pulo
+        if key[pygame.K_w] and self.no_chao:
+            self.gravity = -15
+            self.no_chao = False 
 
+        # Verifica colisoes com plataformas
+        self.no_chao = False
+        for plataforma_ in plataformas:
+            if plataforma_[1].colliderect(self.rect.x, self.rect.y + self.gravity, self.width, self.height):
+                if self.gravity > 0:  # Falling
+                    self.rect.bottom = plataforma_[1].top
+                    self.gravity = 0
+                    self.no_chao = True  
+                elif self.gravity < 0:  
+                    self.rect.top = plataforma_[1].bottom
+                    self.gravity = 0  
 
-                if key[pygame.K_RIGHT]:
-                            
-                    if self.colisao_livro_um == "à esquerda do obstaculo" or self.colisao_placa_um == "à esquerda do obstaculo" or self.colisao_livro_dois == "à esquerda do obstaculo" or self.colisao_placa_dois == "à esquerda do obstaculo":
-                     pass
-                    else:
-                        self.dx += 5
-                         
-                self.vel_y += 1
-                
-                if self.vel_y > 10:
-                    self.vel_y = 10
+        # Aplica gravidade a velocidade
+        if not self.no_chao:
+            self.rect.y += self.gravity
 
-                
-                if colisao_livro_um == "à cima do obstaculo" or colisao_placa_um == "à cima do obstaculo" or colisao_livro_dois == "à cima do obstaculo" or colisao_placa_dois == "à cima do obstaculo":
-                    pass
-                else:
-                    self.dy += self.vel_y
+        # Limita o movimento do boneco
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > 700:
+            self.rect.right = 700
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.gravity = 0
+        if self.rect.bottom > 450:
+            self.rect.bottom = 450
+            self.no_chao = True 
 
-                self.rect.x += self.dx
-
-                if colisao_livro_um == "à cima do obstaculo":
-                    pass
-                else:
-                    self.rect.y += self.dy
-            
-                for plataforma_ in plataformas:
-                    if plataforma_[1].colliderect(self.rect.x + self.dx, self.rect.y, self.width, self.height):
-                        self.dx = 0
-
-                    if plataforma_[1].colliderect(self.rect.x, self.rect.y + self.dy, self.width, self.height):
-                        if self.vel_y < 0:
-                                self.dy = plataforma_[1].bottom - self.rect.top
-                                self.vel_y = 0
-
-                        elif self.vel_y >= 0:
-                                self.dy = plataforma_[1].top - self.rect.bottom
-                                self.vel_y = 0
-
-                self.rect.x += self.dx
-                self.rect.y += self.dy
-
-                if self.rect.bottom > 700: #largura da tela
-                        self.rect.bottom = 450 #altura da tela
-                        self.dy = 0
-
-                                       
-                tela.blit(self.image, self.rect)
+        # Atualiza a tela
+        tela.blit(self.imagem_player, self.rect)
