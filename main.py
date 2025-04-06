9d8f00b85e75ed4672a37fc607b3072d99fee517
romero003 <153675281+romero003@users.noreply.github.com>
2025-04-05 21:44:00 -0300

    verifica toque na agua

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ pygame.display.set_caption('Água e fogo no CIn, sem água e sem fogo')
 qtd_moedas = 0
 qtd_cracha = 0
 qtd_cafe = 0
+grupo_obstaculos = mundo.obstaculos
 grupo_moedas = mundo.moedas
 cracha = mundo.crachas
 cafe = mundo.cafes
@@ -36,65 +37,79 @@ estado_cafe = False
 counter, text = 10, '10'.rjust(3)
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 font = pygame.font.SysFont('Consolas', 30)
-
+perdeu = False
 
 while running:
+    if perdeu == False:
+        relogio.tick(42)
+        mundo.draw()
+        grupo_moedas.draw(tela)
+        cracha.draw(tela)
+        cafe.draw(tela)
 
-    relogio.tick(42)
-    mundo.draw()
-    grupo_moedas.draw(tela)
-    cracha.draw(tela)
-    cafe.draw(tela)
+        jogador.update(mundo,pulo_cafe)
+        
+        #checa se o jogador tocou na água:
+        if pygame.sprite.spritecollide(jogador, grupo_obstaculos, False):
+            perdeu = True
+            print('perdeu mane')
+            jogador.rect.x = 75
+            jogador.rect.y = 515
+            
+        # checa se alguma moeda foi coletada
+        if pygame.sprite.spritecollide(jogador, grupo_moedas, True):
+            qtd_moedas += 1
 
-    jogador.update(mundo,pulo_cafe)
+        # checa se o crachá foi coletado
+        if pygame.sprite.spritecollide(jogador, cracha, True):
+            cracha_coletado = True
+            qtd_cracha += 1
 
-    # checa se alguma moeda foi coletada
-    if pygame.sprite.spritecollide(jogador, grupo_moedas, True):
-        qtd_moedas += 1
+        # checa se o café foi coletado
+        if pygame.sprite.spritecollide(jogador, cafe, True):
+            qtd_antiga = 0
+            cafe_coletado = True
+            qtd_cafe += 1
 
-    # checa se o crachá foi coletado
-    if pygame.sprite.spritecollide(jogador, cracha, True):
-        cracha_coletado = True
-        qtd_cracha += 1
+            #faz a parte do segundo cafe funcionar
+            if qtd_cafe > qtd_antiga:
+                pulo_cafe = True
 
-    # checa se o café foi coletado
-    if pygame.sprite.spritecollide(jogador, cafe, True):
-        qtd_antiga = 0
-        cafe_coletado = True
-        qtd_cafe += 1
+            qtd_antiga = qtd_cafe
 
-        #faz a parte do segundo cafe funcionar
-        if qtd_cafe > qtd_antiga:
-            pulo_cafe = True
+            #pulo do café
+            if pulo_cafe == True:
 
-        qtd_antiga = qtd_cafe
+                for e in pygame.event.get():
+                    if e.type == pygame.USEREVENT: 
+                        counter -= 1
+                        text = str(counter).rjust(3) if counter > 0 else 'boom!'
 
-        #pulo do café
-        if pulo_cafe == True:
+                        if text == 'boom!':
+                            pulo_cafe = False 
+                            counter, text = 10, '10'.rjust(3)
+                            pygame.time.set_timer(pygame.USEREVENT, 1000)
+                    
+                    relogio.tick(60)
+                draw_texto(f'Tempo: {text}',fonte_1,PRETO,10,70,tela)
 
-            for e in pygame.event.get():
-                if e.type == pygame.USEREVENT: 
-                    counter -= 1
-                    text = str(counter).rjust(3) if counter > 0 else 'boom!'
+        # exibir contagem de itens
+        draw_texto(f'Moedas: {qtd_moedas}', fonte_1, PRETO, 10, 10, tela)
+        draw_texto(f'Crachá: {qtd_cracha}', fonte_1, PRETO, 10, 30, tela)
+        draw_texto(f'Cafés: {qtd_cafe}', fonte_1, PRETO, 10, 50, tela)
 
-                    if text == 'boom!':
-                        pulo_cafe = False 
-                        counter, text = 10, '10'.rjust(3)
-                        pygame.time.set_timer(pygame.USEREVENT, 1000)
-                
-                relogio.tick(60)
-            draw_texto(f'Tempo: {text}',fonte_1,PRETO,10,70,tela)
+        pygame.display.update()
 
-    # exibir contagem de itens
-    draw_texto(f'Moedas: {qtd_moedas}', fonte_1, PRETO, 10, 10, tela)
-    draw_texto(f'Crachá: {qtd_cracha}', fonte_1, PRETO, 10, 30, tela)
-    draw_texto(f'Cafés: {qtd_cafe}', fonte_1, PRETO, 10, 50, tela)
+    elif perdeu == True:
+        key = pygame.key.get_pressed()
+        if key[pygame.K_r]:
+            perdeu = False
 
-    # fechar jogo
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    pygame.display.update()
+        tela.blit(jogador.imagem_player, jogador.rect)
+
 
 pygame.quit()
