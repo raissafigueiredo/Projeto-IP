b86da2a365ab50e2b790cc000688355ecc5e5f61
alicemaca <maria.camorim@ufpe.br>
2025-04-07 14:04:48 -0300

    Update globais.py

--- globais.py
+++ globais.py
@@ -7,8 +7,7 @@ ALTURA = 600
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 
 # Definições dos objetos
-TAM_COLECIONAVEIS = (30,30)
-TAM_MOEDAS = (20,28)
+TAM_COLECIONAVEIS = (25,25)
 TAM_OBSTACULOS = (25,25)
 
 # Fontes
@@ -20,15 +19,21 @@ BRANCO = (250, 250, 250)
 PRETO = (0,0,0)
 AZUL_CLARO = (117, 179, 250)
 VERMELHO = (180, 0,0)
+VERMELHO_TESTE = (180,45,20)
 
 # Assets
 cafe_img = load_imagem('cafe.png')
 background_img = load_imagem('Cenário.jpeg')
 cracha_img = load_imagem('cracha.png')
-catraca_img = load_imagem('catraca.png')
 piso_img = load_imagem('piso.png')
 agua_esq_img = load_imagem('agua_esq.png')
 agua_dir_img = load_imagem('agua_dir.png')
 livro_img = load_imagem('livros.png')
 moeda_img = load_imagem('moedas.png')
 player_img = load_imagem('personagem_andar_1.png')
+catraca_img = load_imagem('catraca.png')
+
+porta_img = load_imagem('porta.png')
+robo_cin_img = load_imagem('robo_cin.png')
+janela_img = load_imagem('janela.png')
+jeep_img = load_imagem('pixil-frame-0.png')
